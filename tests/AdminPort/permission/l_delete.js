73c91460abbe538f09881a16615630d1468afdf0
luodan <luodan@elinkport.com>
2018-08-06 10:14:40 +0800

    完善测试

--- tests/AdminPort/permission/l_delete.js
+++ tests/AdminPort/permission/l_delete.js
@@ -12,12 +12,11 @@ module.exports = {
             .assert.urlEquals(client.launchUrl + 'login?redirect=%2F')
             // .waitForElementVisible(mock.nameInput, mock.pauseTime)
             .setValue(mock.nameInput, mock.super_admin)//输入账号
-            .pause(mock.pauseTime)
 
             // .waitForElementVisible(mock.pwdInput, mock.pauseTime)
             .setValue(mock.pwdInput, mock.super_admin_password)//输入密码
             .click(mock.loginBtn)//点击登陆
-
+            .pause(mock.pauseTime)
             // .waitForElementVisible(mock.homePageText, mock.pauseTime)
             .assert.containsText(mock.homePageText, "欢迎使用，优企康管理平台")
             .assert.urlEquals(client.launchUrl)
